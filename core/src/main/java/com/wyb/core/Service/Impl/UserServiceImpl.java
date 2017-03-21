02fbb096dbf20894c033c6f44498472cf0276222
wyb <1501987783@qq.com>
2017-03-21 15:10:02 +0800

    //

---
+++ core/src/main/java/com/wyb/core/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.wyb.core.Service.Impl;
+
+import com.wyb.core.Service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 15019 on 2017/3/20.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+}
---
+++ core/src/main/java/com/wyb/core/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.wyb.core.Service;
+
+/**
+ * Created by 15019 on 2017/3/20.
+ */
+public interface UserService {
+}
